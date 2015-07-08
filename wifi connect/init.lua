7a50e62c66b1798a8994f54c2c63db5c9f4833ba
specas <shrenik95@gmail.com>
2015-07-08 13:17:54 +0530

    second commit

--- wifi connect/init.lua
+++ wifi connect/init.lua
@@ -1,6 +1,6 @@
 print(wifi.sta.getip())
 wifi.setmode(wifi.STATION)
-wifi.sta.config("ScreamingSilence", "Knowledge_Is_P0w3r")
+wifi.sta.config("-----", "-----")
 wifi.sta.connect()
 tmr.delay(1000000)
 print(wifi.sta.getip())
